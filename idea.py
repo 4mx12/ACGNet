f74f752e3f6e6e0d6be25edfefbbf201a8508e2b
Mao xin <125878424+4mx12@users.noreply.github.com>
2025-05-03 16:53:12 +0800

    Network

--- idea.py
+++ idea.py
@@ -1,102 +1,10 @@
-
-
-
 import torch
 import torch.nn as nn
 from einops import rearrange
 import typing as t
-from configchange import opt
 import torch.autograd
-from dyconv import DEConv_2, FCAttention, LRCED, CED
+from dyconv import DEConv_2, FCAttention
 import torch.nn.functional as F
-# import numpy as np
-
-class DynamicWeightFusion(nn.Module):
-    def __init__(self, channels):
-        super(DynamicWeightFusion, self).__init__()
-        self.channels = channels
-        self.weight_conv = nn.Sequential(
-            nn.Conv2d(channels * 2, channels, kernel_size=1, bias=False),
-            nn.ReLU(),
-            nn.Conv2d(channels, 2, kernel_size=1, bias=False),
-            nn.Softmax(dim=1)
-        )
-
-    def forward(self, x1, x2):
-        x = torch.cat([x1, x2], dim=1)  # Concatenate feature maps
-        weights = self.weight_conv(x)   # Predict blending weights
-        x1_weighted = weights[:, 0:1, :, :] * x1
-        x2_weighted = weights[:, 1:2, :, :] * x2
-        return x1_weighted + x2_weighted
-
-class skip_fe_model(nn.Module):
-    def __init__(self, inc, outc):
-        super(skip_fe_model, self).__init__()
-
-        self.conv1 = nn.Conv2d(inc, outc, 3, padding=1)
-        self.conv2 = nn.Conv2d(outc, outc, 3, padding=1)
-        self.conv3 = nn.Conv2d(outc, outc, 3, padding=1)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.relu3 = nn.ReLU(inplace=True)
-
-
-
-
-
-    def forward(self, x):
-        # 通过卷积层 -> 激活函数 -> 池化层
-        conv1 = self.conv1(x)
-        relu1 = self.relu1(conv1)
-        # mp1 = self.avgpool1(relu1)
-        # x1 = mp1
-        x1 = relu1 + x
-
-        conv2 = self.conv2(x1) + x + x1
-        relu2 = self.relu2(conv2)
-        # mp2 = self.avgpool2(relu2)
-        # x2 = mp2
-        x2 = relu2 + x + x1
-        conv3 = self.conv3(x2) + x + x1 + x2
-        relu3 = self.relu3(conv3)
-        # mp3 = self.avgpool3(relu3)
-        # x3 = mp3
-        x3 = relu3 + x + x1 + x2
-
-        return x3
-
-class single_conv_res(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(single_conv_res, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 1, bias=False),
-            nn.ReLU(inplace=True)
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(out_ch, out_ch, 3, padding=1, bias=False),
-            nn.ReLU(inplace=True)
-        )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(out_ch, out_ch, 1, bias=False)
-        )
-
-        if in_ch != out_ch:
-            self.sc = nn.Sequential(
-                nn.Conv2d(in_ch, out_ch, kernel_size=1, bias=False),
-            )
-        else:
-            self.sc = nn.Sequential()
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-
-        out1 = self.conv1(x)
-        out2 = self.conv2(out1)
-        out3 = self.conv3(out2)
-
-        out = out3 + self.sc(x)
-
-        return self.relu(out)
 
 
 class single_conv(nn.Module):
@@ -128,7 +36,6 @@ class skip_connect(nn.Module):
         return x
 
 
-
 class Up(nn.Module):
     def __init__(self, in_ch):
         super(Up, self).__init__()
@@ -148,92 +55,6 @@ class outconv(nn.Module):
         x = self.conv(x)
         return x
 
-class ChannelAttentionModule(nn.Module):
-    def __init__(self, channel, ratio=16):
-        super(ChannelAttentionModule, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
-
-        self.shared_MLP = nn.Sequential(
-            nn.Conv2d(channel, channel // ratio, 1, bias=False),
-            nn.ReLU(),
-            nn.Conv2d(channel // ratio, channel, 1, bias=False)
-        )
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        avgout = self.shared_MLP(self.avg_pool(x))
-        maxout = self.shared_MLP(self.max_pool(x))
-        return self.sigmoid(avgout + maxout)
-
-class SpatialAttentionModule(nn.Module):
-    def __init__(self):
-        super(SpatialAttentionModule, self).__init__()
-        self.conv2d = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        avgout = torch.mean(x, dim=1, keepdim=True)
-        maxout, _ = torch.max(x, dim=1, keepdim=True)
-        out = torch.cat([avgout, maxout], dim=1)
-        out = self.sigmoid(self.conv2d(out))
-        return out
-
-class CBAM(nn.Module):
-    def __init__(self, channel):
-        super(CBAM, self).__init__()
-        self.channel_attention = ChannelAttentionModule(channel)
-        self.spatial_attention = SpatialAttentionModule()
-
-
-
-    def forward(self, x):
-        out = self.channel_attention(x) * x
-        out = self.spatial_attention(out) * out
-        return out
-
-
-class CSDN_Tem(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(CSDN_Tem, self).__init__()
-        self.depth_conv = nn.Conv2d(
-            in_channels=in_ch,
-            out_channels=in_ch,
-            kernel_size=7,  # 修改卷积核大小为7x7
-            stride=1,
-            padding=3,  # 调整padding以保持输出尺寸不变
-            groups=in_ch  # 使用分组卷积，每个输入通道独立卷积
-        )
-        self.point_conv = nn.Conv2d(
-            in_channels=in_ch,
-            out_channels=out_ch,
-            kernel_size=1,  # 逐点卷积使用1x1卷积核
-            stride=1,
-            padding=0,
-            groups=1  # 不使用分组
-        )
-
-    def forward(self, input):
-        out = self.depth_conv(input)
-        out = self.point_conv(out)
-        return out
-
-
-class CEBlock(nn.Module):
-    def __init__(self,
-                 in_channels, out_channels,
-                 group=1):
-        super(CEBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels,  out_channels, 1)
-        self.conv2 = nn.Conv2d(in_channels,  out_channels, 1)
-        self.conv3 = nn.Conv2d(in_channels,  4*out_channels, 1)
-        self.conv4 = nn.Conv2d(4*out_channels, out_channels, 1)
-        self.body1 = nn.Sequential(self.conv1,nn.GELU(), CSDN_Tem(in_channels,  out_channels), nn.GELU(), self.conv2
-                                   ,nn.GELU(),  self.conv3, nn.GELU(),  self.conv4, nn.GELU())
-
-    def forward(self, x):
-        out = self.body1(x)
-        return out
 
 class ResidualBlock(nn.Module):
     def __init__(self,
@@ -250,26 +71,6 @@ class ResidualBlock(nn.Module):
         out = self.body(x)
         out = F.relu(out + x)
         return out
-
-class ConvBlock(nn.Module):
-    def __init__(self, in_size, out_size, relu_slope):
-        super(ConvBlock, self).__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(in_size, out_size, kernel_size=3, padding=1, bias=True),
-            nn.LeakyReLU(relu_slope),
-            nn.Conv2d(out_size, out_size, kernel_size=3, padding=1, bias=True),
-            nn.LeakyReLU(relu_slope))
-
-        self.shortcut = nn.Conv2d(in_size, out_size, kernel_size=1, bias=True)
-
-    def forward(self, x):
-        out = self.block(x)
-        sc = self.shortcut(x)
-        out = out + sc
-        return out
-
-
-
 
 
 class SCSA(nn.Module):
@@ -396,7 +197,6 @@ class SCSA(nn.Module):
         return attn * x
 
 
-
 class Deconv_im(nn.Module):
     def __init__(self, dim):
         super(Deconv_im, self).__init__()
@@ -428,40 +228,35 @@ class UNet(nn.Module):
 
             single_conv(1, 32),
             single_conv(32, 32),
-            # Deconv_im(32),
+            Deconv_im(32),
 
         )
 
         self.down1 = nn.Sequential(nn.Conv2d(32, 64, 3, padding=1, stride=2), nn.ReLU(inplace=True))
 
-        self.pool1 = nn.AvgPool2d(1)
         self.conv1 = nn.Sequential(
             single_conv(64, 64),
             single_conv(64, 64),
-            # Deconv_im(64),
+            Deconv_im(64),
 
         )
 
         self.down2 = nn.Sequential(nn.Conv2d(64,128, 3, padding=1, stride=2), nn.ReLU(inplace=True))
 
-
         self.conv2 = nn.Sequential(
             single_conv(128, 128),
             single_conv(128, 128),
-
-            # Deconv_im(128),
+            Deconv_im(128),
 
         )
 
         self.down3 = nn.Sequential(nn.Conv2d(128, 256, 3, padding=1, stride=2), nn.ReLU(inplace=True))
 
-
-
         self.up1 = Up(256)
         self.conv4 = nn.Sequential(
             single_conv(128,128),
             single_conv(128,128),
-            # Deconv_im(128),
+            Deconv_im(128),
 
         )
 
@@ -469,31 +264,26 @@ class UNet(nn.Module):
         self.conv5 = nn.Sequential(
             single_conv(64,64),
             single_conv(64,64),
-            # Deconv_im(64)
+            Deconv_im(64)
         )
 
         self.up3 = Up(64)
         self.conv6 = nn.Sequential(
             single_conv(32,32),
             single_conv(32,32),
-            # Deconv_im(32),
+            Deconv_im(32),
 
         )
 
         self.outc = outconv(32, 1)
 
-
-
         self.skip1 = skip_connect()
         self.skip2 = skip_connect()
         self.skip3 = skip_connect()
 
-
         self.mu_1 = nn.Parameter(torch.FloatTensor(1), requires_grad=True)
-        # self.gamma_1 = nn.Parameter(torch.FloatTensor(1), requires_grad=True)
-
         self.mu_1.data = torch.tensor(0.5)
-        # self.gamma_1.data = torch.tensor(0.2)
+
 
 
     def forward(self, input):
@@ -501,162 +291,68 @@ class UNet(nn.Module):
         y = input
         for i in range(2):
             x1 = self.CG_optim(x1, y)
+
             inx = self.inc(x1)
-
             down1 = self.down1(inx)
-        # down1 = self.hlfd0(down1) + down1
+
             conv1 = self.conv1(down1)
-        # conv1 = self.multi2(conv1) + conv1
-        # at0 = self.ca_block1(conv1) + conv1
-        # fe1 = self.fe1(conv1)
-        # conv1 = self.pool1(conv1)
-
-
             down2 = self.down2(conv1)
-        # down2 = self.hlfd1(down2) + down2
+
             conv2 = self.conv2(down2)
-
-
             down3 = self.down3(conv2)
-        # down3 = self.hlfd2(down3) + down3
-        #     conv3 = self.conv3(down3)
-        # conv3 = self.multi4(conv3) +  conv3
-        # at2 = self.ca_block3(conv3) + conv3
-        # conv3 = self.ca_block3(conv3) + conv3
-        # conv3 = self.pool3(conv3)
-
 
             up1 = self.up1(down3)
             up1 = self.skip1(up1, conv2)
-        # up1 = self.multi5(up1) + up1
-        # up1 = self.multi2([inx, conv1, conv2], 2) + up1
-
             conv4 = self.conv4(up1)
-        # at3 = self.ca_block4(conv4) + conv4
-
-        # conv4 = self.ehance_block1(conv4)
-        # fe4 = self.fe4(conv4)
-        # at4 = self.ca_block4(conv4)
 
             up2 = self.up2(conv4)
             up2 = self.skip2(up2, conv1)
-        # up2 = self.multi6(up2) + up2
-        # up2 = self.multi1([inx, conv1, conv2], 1) + up2
-
             conv5 = self.conv5(up2)
-        # at4 = self.ca_block5(conv5) + conv5
-        # conv5 = self.ehance_block2(conv5)
-        # fe5 = self.fe5(conv5)
-        # at5 = self.ca_block5(conv5)
 
             up3 = self.up3(conv5)
             up3 = self.skip3(up3, inx)
-        # up3 = self.multi7(up3) + up3
-        # up3 = self.multi0([inx, conv1, conv2], 0) + up3
             conv6 = self.conv6(up3)
-        # conv6 = self.ehance_block3(conv6)
-        # fe6 = self.fe6(conv6)
-        # at6 = self.ca_block6(conv6)
 
-        # out = self.ehance_block(conv6)
             res = self.outc(conv6)
             x1 = res + x1
-            # out = self.reconnect(v, y)
+
 
         return x1
 
-    # def CG_optim(self, u_k, y):
-    #     mu = self.mu_1
-    #     u = torch.zeros_like(y)
-    #     H = 1 + mu  # 系数矩阵 H，因为 K 是单位矩阵
-    #     b = y + mu * u_k  # 计算 b
-    #     r = b - H * u  # 初始残差 r0
-    #     p = r.clone()  # 初始搜索方向 p0
-    #
-    #     # 单次迭代
-    #     for _ in range(2):
-    #         # 计算 alpha_k
-    #         r_norm_sq = torch.sum(r * r, dim=list(range(1, r.dim())), keepdim=True)
-    #         Hp = H * p
-    #         pHp = torch.sum(p * Hp, dim=list(range(1, p.dim())), keepdim=True)
-    #         alpha = r_norm_sq / (pHp + 1e-10)
-    #
-    #         # 更新解向量 u
-    #         u = u + alpha * p
-    #
-    #         # 更新残差 r
-    #         r_new = r - alpha * Hp
-    #
-    #         # 计算 beta_k
-    #         r_new_norm_sq = torch.sum(r_new * r_new, dim=list(range(1, r_new.dim())), keepdim=True)
-    #         beta = r_new_norm_sq / (r_norm_sq + 1e-10)
-    #
-    #         # 更新搜索方向 p
-    #         p = r_new + beta * p
-    #
-    #         # 更新残差
-    #         r = r_new
-    #
-    #     return u
 
     def CG_optim(self, u_k, y):
         mu = self.mu_1
-        # gamma = self.gamma_1
         u = torch.zeros_like(y)
-        H = 1 + mu  # 系数矩阵 H
-        # 初始化残差和搜索方向
+        H = 1 + mu  #  H
+
         r = y + mu * u_k - H * u
         p = r.clone()
-        for _ in range(15):  # 您可以根据需要调整迭代次数
-            # 计算 H * p
+        for _ in range(15):  #  adjust the number of iterations as needed
+            # compute H * p
             Hp = H * p
 
-            # 计算步长 alpha_k
-            numerator = (r * r).sum(dim=(1, 2, 3), keepdim=True)  # 分子：r^T * r
-            denominator = (p * Hp).sum(dim=(1, 2, 3), keepdim=True) + 1e-9  # 分母：p^T * H * p，防止除零
+            # Compute alpha_k
+            numerator = (r * r).sum(dim=(1, 2, 3), keepdim=True)
+            denominator = (p * Hp).sum(dim=(1, 2, 3), keepdim=True) + 1e-9  
             alpha = numerator / denominator  # alpha_k = (r^T * r) / (p^T * H * p)
 
-            # 更新解向量 u_{k+1}
-            u = u + alpha * p # u_{k+1} = u_k + alpha_k * p_k
+            # Update u_k
+            u = u + alpha * p # u_k+1 = u_k + alpha_k * p_k
 
-            # 更新残差 r_{k+1}
-            r_new = r - alpha * Hp# r_{k+1} = r_k - alpha_k * H * p_k
+            # Update r_k+1
+            r_new = r - alpha * Hp # r_{k+1} = r_k - alpha_k * H * p_k
 
-            # 计算 beta_k
-            numerator_new = (r_new * r_new).sum(dim=(1, 2, 3), keepdim=True)  # 分子：r_{k+1}^T * r_{k+1}
+            # Compute beta_k
+            numerator_new = (r_new * r_new).sum(dim=(1, 2, 3), keepdim=True) 
             beta = numerator_new / (numerator + 1e-9)  # beta_k = (r_{k+1}^T * r_{k+1}) / (r_k^T * r_k)
 
-            # 更新搜索方向 p_{k+1}
-            p = r_new + beta * p  # p_{k+1} = r_{k+1} + beta_k * p_k
+            # Update p_k+1
+            p = r_new + beta * p  # p_k+1 = r_k+1 + beta_k * p_k
 
-            # 为下一次迭代更新变量
             r = r_new.clone()
 
 
-        # # 共轭梯度法更新步骤
-        # for _ in range(1):  # 单次迭代
-        #         # 计算步长 alpha(k)
-        #     alpha = torch.mul(r.transpose(2,3) , r) / torch.mul(p.transpose(2,3), (H * p))
-        #         # r1 = r - gamma*torch.mul(alpha , p)
-        #         # p1 = r1.clone()
-        #         # alpha1 = torch.mul(r1.transpose(2,3) , r1) / torch.mul(p1.transpose(2,3) , (H * p1))
-        #         # u1 = u + torch.mul(alpha1 , p)
-        #     u1 = u + torch.mul(alpha, p)
-
-                # # 更新残差 r(k+1)
-            # r_new = r - (alpha * H * p + gamma*torch.mul(alpha, p)) + 1e-8
-            #     #
-            #     # # 计算 beta(k)
-            # beta = torch.mul(r_new.transpose(2,3) , r_new) / torch.mul(r.transpose(2,3) , r)
-            #
-            #     # # 更新搜索方向 p(k+1)
-            # p = r_new + beta * p
-            #     #
-            #     # # 更新残差为新的残差
-            # r = r_new
-
         return u
-
 
 
 
@@ -682,6 +378,4 @@ if __name__ == '__main__':
     net = Network().cuda()
 
     out= net(input1)
-    # print(net)
     print(out.size())
-    # print(out2.size())
