10d47055ff45f0e6cd216b1a52bf0f858904ac3d
Mao xin <125878424+4mx12@users.noreply.github.com>
2025-05-03 16:54:49 +0800

    train config

--- configchange.py
+++ configchange.py
@@ -3,11 +3,10 @@ class DefaultConfig(object):
 
 
     data_root = r'./train/train'
-    # data_root = '/home/yuzhang/experiment/hzf/DPDNN_PyTorch-master/data/RS_data'
-    # label_root = './data/label_128'
+
     num_data = 20000
     crop_size = 128
-    noise_level = 8
+    noise_level = 1
     batch_size = 16  # batch size
     use_gpu = True  # user GPU or not
     num_workers = 1  # how many workers for loading data
@@ -15,15 +14,14 @@ class DefaultConfig(object):
     max_epoch = 50
     lr = 0.0005
     lr_decay = 0.5
-    teacher_model_path = r'./checkpoints/model_201_sigma50.pth'
+
     load_model_path = r'./checkpoints/base_L%d.pth'%noise_level
     save_model_path = r'./checkpoints/base_L%d_no256_no_dega.pth'%noise_level
 
-  #  load_model_path = '/home/yuzhang/experiment/hzf/DPDNN_PyTorch-master/DENOISE/checkpoints/DPDNN_denoischangenew_sigma%d.pth'% noise_level
-   # save_model_path = '/home/yuzhang/experiment/hzf/DPDNN_PyTorch-master/DENOISE/checkpoints/DPDNN_denoischangenew_sigma%d.pth'% noise_level
+
 
 opt = DefaultConfig()
-#之前是加了BN层的
+
 
 
 
